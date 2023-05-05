d35a1ba4cfb7300ce84476356ad63dba89229669
Andrey <kovorot0875@mail.ru>
2023-05-05 08:02:27 +0300

    Gradle added

---
+++ db/src/main/java/ru/netology/DbSettings.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class DbSettings {
+
+    private String name;
+    private String password;
+
+    public DbSettings(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
